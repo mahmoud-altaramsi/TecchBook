6887d404902b0c0ca5a0e6bbc05e0124a841c7ef
mahmoud-altaramsi <mahmoudkhaledaltaramsi@gmail.com>
2024-05-21 01:24:36 +0300

    creat style app

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -6,4 +6,14 @@ abstract class Styles {
     fontWeight: FontWeight.w600,
     color: Colors.white,
   );
+  static const textStyle18 =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+  static const textStyle20 =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
+  static const textStyle30 =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.normal);
+  static const textStyle14 =
+      TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+  static const textStyle16 =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
 }
