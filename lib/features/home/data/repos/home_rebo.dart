2541f966cad9c1813c8fd5db2ce77e8318329b1f
mahmoud-altaramsi <mahmoudkhaledaltaramsi@gmail.com>
2024-06-14 21:13:45 +0300

    creat failure class and get either packedg

--- lib/features/home/data/repos/home_rebo.dart
+++ lib/features/home/data/repos/home_rebo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:tech_book/core/errors/failure.dart';
+import 'package:tech_book/features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSelerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure,List<BookModel>>> fetchBestSelerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
