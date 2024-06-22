cc1d2d76a7038ec9f1c361ef3900550a751dd653
mahmoud-altaramsi <mahmoudkhaledaltaramsi@gmail.com>
2024-06-22 04:01:01 +0300

    featch issus with book model add space

--- lib/features/home/data/repos/home_reop_impl.dart
+++ lib/features/home/data/repos/home_reop_impl.dart
@@ -31,7 +31,7 @@ class HomeReboImpl implements HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
     try {
       var data = await apiService.get(
-          endPoint: 'volumes?Filtering-free-ebooks&q=subject: Programming');
+          endPoint: 'volumes?Filtering-free-ebooks&q=subject:Programming');
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
